b73945d58529201ff087a46e3fb45bfc3aa94042
Jashgamer69 <102601888+Jashgamer69@users.noreply.github.com>
2023-06-09 22:37:22 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: '6040600851:AAEa39MhdOdLX3Th-cXK4RO-Q3DzbvC45d4',
+    admins: [1774613316],
+    mongo_url: 'mongodb+srv://CryptoBots:CryptoBots@cluster0.jmli7fs.mongodb.net/?retryWrites=true&w=majority',
+    web: 'https://ZombiePepe.site', 
+    yut: 'https://youtube.com/@ZombiePepeNews', 
+    twtr: 'https://twitter.com/ZombiePepeNews', 
+    achannel: 'https://t.me/ZombiePepeNews', 
+    agroup: 'https://t.me/ZombiePepeNews'
+}
